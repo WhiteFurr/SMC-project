a49a493f6163f96c482277b64b41acbf8665435d
WhiteFurr <farhanamujawar25@gmail.com>
2026-02-07 10:47:53 +0530

    Fixed CORAs origin fro vercel deployement

--- backend/app/main.py
+++ backend/app/main.py
@@ -11,11 +11,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "https://solapur-traffic-engine.vercel.app"
-    ],
+    allow_origins=["*"],                  # "http://localhost:8080" # "http://127.0.0.1:80  # "https://solapur-traffic-engine.vercel.app"
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
